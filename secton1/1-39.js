9ed82341912fab6870a762abcf35070e7fb5a793
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-01 00:27:18 +0900

    1-39修正

--- secton1/1-39.js
+++ secton1/1-39.js
@@ -1,14 +1,28 @@
-function tan_cf(x, k) {
-  function cont_frac(n, d, k) {
-    function fraction(i) {
-      return i > k  
-        ? 0
-        : n(i) / (d(i) - x * fraction(i+1));
-    }
-    return fraction(k);
+// function tan_cf(x, k) {
+//   function cont_frac(n, d, k) {
+//     function fraction(i) {
+//       return i > k  
+//         ? 0
+//         : n(i) / (d(i) - x * fraction(i+1));
+//     }
+//     return fraction(k);
+//   }
+//   return cont_frac(i=>x, i=>1+2*i, 0);
+// }
+
+function cont_frac(n, d, k) {
+  function fraction(i) {
+    return i > k  
+      ? 0
+      : n(i) / (d(i) + fraction(i+1));
   }
-  return cont_frac(i=>x, i=>1+2*i, 0);
+  return fraction(1);
 }
 
+function tan_cf(x, k) {
+  return cont_frac( i => i===1 ? x : -x*x,
+                    i => 2 * i - 1,
+                    k);
+}
 console.log(tan_cf(Math.PI, 14));
 
