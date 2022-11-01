64e38da5b9ddf9dbc9f8fde40067e1a7f8eedec4
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-01 09:31:46 +0900

    1-46

--- secton1/1-42.js
+++ secton1/1-42.js
@@ -10,7 +10,87 @@ function inc(x) {
   return x + 1;
 }
 
-const value = compose(square, inc)(6);
-console.log(value);
+{
+  console.log('--1-42--');
+  const value = compose(square, inc)(6);
+  console.log(value);
+}
+
+// 1-43
+function repeated(func, repeat_num) {
+  var retval = func;
+  for(var i = 0; i < repeat_num; ++i) {
+    retval = compose(func, func);
+  }
+  return retval;
+}
+
+{
+  console.log('--1-43--');
+  const value = repeated(square, 2)(5);
+  console.log(value);
+}
 
 
+// 1-44
+const dx = 0.00001;
+
+function smooth(f) {
+  return x => (f(x-dx) + f(x) + f(x+dx)) / 3;
+}
+
+
+{
+  console.log('--1-44--');
+  const value = smooth(square)(6);
+  console.log(value);
+}
+
+
+// 1-45
+const tolerance = 0.00001;
+function fixed_point(f, first_guess) {
+    function close_enough(x, y) {
+        return abs(x - y) < tolerance;
+    }
+    function try_with(guess) {
+        const next = f(guess);
+        return close_enough(guess, next)
+               ? next
+               : try_with(next);
+    }
+    return try_with(first_guess);
+}
+function abs(x) {
+  return x < 0
+    ? -x
+    : x;
+}
+function average_damp(f) {
+  return x => average(x, f(x));
+}
+function average(a,b) {
+  return (a + b) / 2;
+}
+
+
+// 1-46
+function iterative_improve(good_enough, improve) {
+  function iterate(x) {
+    return good_enough(x)
+    ? x
+    : iterate(improve(x));
+  }
+  return iterate;
+}
+
+function sqrt_iter(x) {
+  return iterative_improve(
+    y => (y < tolerance),
+    y => average_damp(y, n => n/y),
+  )(x);
+}
+
+{
+  console.log(sqrt_iter(5));
+}
