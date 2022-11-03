01a2801c5533e45443caec5b436532d82ec445e4
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-03 20:11:59 +0900

    2-10

--- section2/2-7.js
+++ section2/2-7.js
@@ -1,11 +1,11 @@
 function pair(a, b) {
-  return {i:a, j:b};
-}
-function head(p) {
-  return p.i;
+  return {low:a, high:b};
 }
 function tail(p) {
-  return p.j;
+  return p.high;
+}
+function head(p) {
+  return p.low;
 }
 
 //2-7
@@ -14,10 +14,10 @@ function make_interval(x, y) {
   return pair(x, y);
 }
 function lower_bound(interval) {
-  return tail(interval);
+  return head(interval);
 }
 function upper_bound(interval) {
-  return head(interval);
+  return tail(interval);
 }
 
 {
@@ -41,3 +41,32 @@ function sub_interval(x, y) {
   const sub = sub_interval(a, b);
   console.log(lower_bound(sub) + ' ' + upper_bound(sub));
 }
+
+
+//2-10
+console.log('--- 2-10 ---');
+function mul_interval(x, y) {
+  const p1 = lower_bound(x) * lower_bound(y);
+  const p2 = lower_bound(x) * upper_bound(y);
+  const p3 = upper_bound(x) * lower_bound(y);
+  const p4 = upper_bound(x) * upper_bound(y);
+  return make_interval(Math.min(p1, p2, p3, p4),
+                       Math.max(p1, p2, p3, p4));
+}
+
+function div_interval(x, y) {
+  if(upper_bound(y) >= 0 && lower_bound(y) <= 0) {
+    console.log('Error');
+    return;
+  }
+  return mul_interval(x, make_interval(1 / upper_bound(y),
+                                       1 / lower_bound(y)));
+}
+
+{
+  const x = make_interval(1, 4);
+  const y = make_interval(-2, 4);
+  const div = div_interval(x, y);
+  console.log(div);
+}
+
