bc8b2185329a3c9c49b43e859ebc6b9872bfebe4
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-03 20:02:25 +0900

    2-7

--- section2/2-7.js
+++ section2/2-7.js
@@ -9,6 +9,7 @@ function tail(p) {
 }
 
 //2-7
+console.log('--- 2-7 ---');
 function make_interval(x, y) {
   return pair(x, y);
 }
@@ -19,7 +20,24 @@ function upper_bound(interval) {
   return head(interval);
 }
 
-const x = make_interval(100, 101);
-console.log(upper_bound(x));
-console.log(lower_bound(x));
+{
+  const x = make_interval(100, 101);
+  console.log(upper_bound(x));
+  console.log(lower_bound(x));
+}
 
+
+// 2-8
+console.log('--- 2-8 ---');
+function sub_interval(x, y) {
+  const max = upper_bound(x) - lower_bound(y);
+  const min = lower_bound(x) - upper_bound(y);
+  return make_interval(min, max);
+}
+
+{
+  const a = make_interval(10, 20);
+  const b = make_interval(2, 4);
+  const sub = sub_interval(a, b);
+  console.log(lower_bound(sub) + ' ' + upper_bound(sub));
+}
