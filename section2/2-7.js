89d491b805fde390b987ec9f08147e89c5ff0d87
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-03 19:39:59 +0900

    微修正

--- section2/2-7.js
+++ section2/2-7.js
@@ -12,16 +12,10 @@ function tail(p) {
 function make_interval(x, y) {
   return pair(x, y);
 }
-function upper_bound(interval) {
-  if(head(interval) > tail(interval)) {
-    return head(interval);
-  }
+function lower_bound(interval) {
   return tail(interval);
 }
-function lower_bound(interval) {
-  if(head(interval) >= tail(interval)) {
-    return tail(interval);
-  }
+function upper_bound(interval) {
   return head(interval);
 }
 
