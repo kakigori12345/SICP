92acf1e844efc51dd6e449c79cc939ea7027a9f0
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-03 22:59:31 +0900

    2-14 途中

--- section2/2-12.js
+++ section2/2-12.js
@@ -51,3 +51,45 @@ function percent(i) {
   const center = make_center_width(10, 1);
   console.log(percent(center));
 }
+
+
+// 2-14
+console.log('------ 2-14 ------');
+
+function print_interval(i) {
+  return "[ "  + lower_bound(i) + 
+         " , " + upper_bound(i) + " ]";
+}
+
+function add_interval(x, y) {
+  return make_interval(lower_bound(x) + lower_bound(y),
+                       upper_bound(x) + upper_bound(y));
+}
+
+function mul_interval(x, y) {
+  const p1 = lower_bound(x) * lower_bound(y);
+  const p2 = lower_bound(x) * upper_bound(y);
+  const p3 = upper_bound(x) * lower_bound(y);
+  const p4 = upper_bound(x) * upper_bound(y);
+  return make_interval(Math.min(p1, p2, p3, p4),
+                       Math.max(p1, p2, p3, p4));
+}
+
+function div_interval(x, y) {
+  return mul_interval(x, make_interval(1 / upper_bound(y),
+                                       1 / lower_bound(y)));
+}
+
+function par1(r1, r2) {
+  return div_interval(mul_interval(r1, r2),
+                      add_interval(r1, r2));
+}
+function par2(r1, r2) {
+  const one = make_interval(1, 1);
+  return div_interval(one,
+                      add_interval(div_interval(one, r1),
+                                   div_interval(one, r2)));
+}
+
+console.log(print_interval(par1(pair(4, 6), pair(7, 8))));
+console.log(print_interval(par2(pair(4, 6), pair(7, 8))));
