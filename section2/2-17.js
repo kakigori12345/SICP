233275b12347bcc43e576414d123a93f3a92cb13
Froslass Glalie <51141586+kakigori12345@users.noreply.github.com>
2022-11-05 09:15:11 +0900

    2-18

--- section2/2-17.js
+++ section2/2-17.js
@@ -77,13 +77,48 @@ console.log('------ 2-17 ------');
 function last_pair(items) {
   // ↓これだと最後の値しか返せなかった
   // return list_ref(items, length(items)-1);
-  
+
   if( is_null(tail(items)) ) {
     return items;
   }
   return last_pair(tail(items));
 }
+
 {
   const last = last_pair(list(23, 72, 149, 34));
   console.log(last);
+}
+
+
+// 2-18
+console.log('------ 2-18 ------');
+
+function remove_tail(items) {
+  if(items === null) {
+    console.log('ERROOOROROR!!!!!!!!!!!!!!!!!!!!!!');
+    return null;
+  }
+
+  return is_null(tail(items))
+    ? null
+    : pair(head(items), remove_tail(tail(items)));
+}
+
+function reverse(items) {
+  return is_null(tail(items))
+    ? pair(head(items), null)
+    : pair( head(last_pair(items)), reverse(remove_tail(items)));
+}
+
+function get_string(items) {
+  return is_null(items)
+    ? ''
+    : head(items) + ' ' + get_string(tail(items));
+}
+
+{
+  const list_ = list(23, 72, 149, 23, 43, 5, 5, 23, 2323, 434);
+  const reverse_ = reverse(list_);
+  console.log( get_string(list_) );
+  console.log( get_string(reverse_) );
 }
